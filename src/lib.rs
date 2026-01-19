d175ef786b60d70445ee54633460006b493118f3
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-18 23:12:24 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![no_std]
 #![cfg_attr(feature = "type_const", allow(incomplete_features))]
 #![cfg_attr(feature = "type_const", feature(min_generic_const_args, register_tool))]
 #![cfg_attr(feature = "type_const", register_tool(type_const))] // Make rust analyzer not show this an error.

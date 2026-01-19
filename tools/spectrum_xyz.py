66c11a8bb9c3d2f6103929e1bc7fc5f4f77d42e7
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-18 16:13:33 -0800

    Calculate D65 white from raw data.

--- tools/spectrum_xyz.py
+++ tools/spectrum_xyz.py
@@ -24,6 +24,42 @@ def load_csv(path, cols):
             data[key] = row
     return data
 
+def calc(spec, cmf):
+    x_raw = 0.0
+    y_raw = 0.0
+    z_raw = 0.0
+    for wl, p in spec.items():
+        p = p[0]
+        if wl not in cmf:
+            continue
+        # http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
+        xbar, ybar, zbar = cmf[wl]
+        x_raw += xbar * p;
+        y_raw += ybar * p;
+        z_raw += zbar * p;
+    # Normalize to Y
+    ratio = 1.0 / y_raw
+    return (x_raw * ratio, 1.0, z_raw * ratio)
 
+def xyz_to_xyY(c):
+    # http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
+    denom = c[0] + c[1] + c[2]
+    x = c[0] / denom
+    y = c[1] / denom
+    Y = c[1]
+    return (x, y, Y)
+
+def print_white_point(name, xyz, xyY):
+    print("~~~~~~~ " + name + " ~~~~~~~")
+    print("================================")
+    print("White Point XYZ\n    X: {:.16f}\n    Y: {:.16f}\n    Z: {:.16f}".format(xyz[0], xyz[1], xyz[2]))
+    print("White Point Chromaticity\n    x: {:.16f}\n    y: {:.16f}\n    Y: {:.16f}".format(xyY[0], xyY[1], xyY[2]))
+
+
+# https://cie.co.at/datatable/cie-standard-illuminant-d65
 spec = load_csv("./CIE_std_illum_D65.csv", 2)
+# https://cie.co.at/datatable/cie-1931-colour-matching-functions-2-degree-observer
 cmf = load_csv("./CIE_xyz_1931_2deg.csv", 4)
+xyz = calc(spec, cmf)
+xyY = xyz_to_xyY(xyz)
+print_white_point("D65 2 Degree FOV", xyz, xyY)

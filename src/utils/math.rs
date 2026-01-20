20d6f38bba2cb4631ae99b621148ed9c8c981375
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-20 10:35:56 -0800

    Update math.rs

--- src/utils/math.rs
+++ src/utils/math.rs
@@ -1,6 +1,13 @@
+const F32_BIAS: i32 = 127;
+
+const fn exponent(bits: u32) -> i32 {
+    return ((bits >> 23) & 0xff) as i32 - F32_BIAS;
+}
+
 /// Computes the quintic root or 5th root.
 pub const fn quirt(x: f32) {
     let bits = x.to_bits();
+    let e = exponent(bits);
     // TODO
     // for rough first guess
     // * divide f32 exponent by 5
@@ -8,4 +15,25 @@ pub const fn quirt(x: f32) {
     // * This is because a f32 is basiclly m * 2^k and (m * 2^k)^(1/5)
     //   is m^(1/5) * 2^(k/5)
     // Apply some number of rounds of Newton's or Halley's method
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn check_exponent() {
+        let mut v: f32 = 1.0;
+        assert_eq!(exponent(v.to_bits()), 0);
+
+        for i in 1..=128i32 {
+            v *= 2.0;
+            assert_eq!(exponent(v.to_bits()), i);
+        }
+
+        v = 1.0;
+        for i in 1..=127 {
+            v *= 0.5;
+            assert_eq!(exponent(v.to_bits()), -i);
+        }
+    }
 }

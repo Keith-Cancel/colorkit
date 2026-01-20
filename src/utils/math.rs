49fa2691d0767539e136a5eeece1cf94dd1e732c
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-20 00:37:14 -0800

    Update math.rs

--- src/utils/math.rs
+++ src/utils/math.rs
@@ -5,5 +5,7 @@ pub const fn quirt(x: f32) {
     // for rough first guess
     // * divide f32 exponent by 5
     // * apply some linear/cheap approx to m^(1/5) of the mantissa.
+    // * This is because a f32 is basiclly m * 2^k and (m * 2^k)^(1/5)
+    //   is m^(1/5) * 2^(k/5)
     // Apply some number of rounds of Newton's or Halley's method
 }

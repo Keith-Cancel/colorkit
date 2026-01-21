d814ab66607e25d9c6183ba253e69896fa1851f6
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-21 12:59:28 -0800

    Update math_fn.rs

--- test_math/src/math_fn.rs
+++ test_math/src/math_fn.rs
@@ -2,6 +2,7 @@ use rug::Float;
 /// Single Agrument Math functions
 pub trait MathFn {
     const NAME: &'static str;
+    fn new() -> Self;
     fn test_f32_impl(x: f32) -> f32;
     fn std_f32_impl(x: f32) -> f32;
     fn rug_impl(x: f32) -> Float;

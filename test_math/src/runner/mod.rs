1fd94c4c0215913faaea7821f7baaec68d45125a
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-21 14:57:12 -0800

    Update mod.rs

--- test_math/src/runner/mod.rs
+++ test_math/src/runner/mod.rs
@@ -1,3 +1,16 @@
 mod perf;
 
 pub use perf::Perf;
+
+pub struct AnsiColors;
+impl AnsiColors {
+    pub const RESET: &'static str = "\x1b[0m";
+    pub const BOLD: &'static str = "\x1b[1m";
+    pub const DIM: &'static str = "\x1b[2m";
+    pub const RED: &'static str = "\x1b[31m";
+    pub const GREEN: &'static str = "\x1b[32m";
+    pub const YELLOW: &'static str = "\x1b[33m";
+    pub const BLUE: &'static str = "\x1b[34m";
+    pub const MAGENTA: &'static str = "\x1b[35m";
+    pub const CYAN: &'static str = "\x1b[36m";
+}

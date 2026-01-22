570ac31585ec839b6c99d912069f7281e415e178
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-22 01:05:18 -0800

    Update mod.rs

--- test_math/src/runner/mod.rs
+++ test_math/src/runner/mod.rs
@@ -38,3 +38,53 @@ impl Iterator for PowersF32 {
         return Some(n);
     }
 }
+
+pub struct RandomF32([u64; 2], usize);
+
+impl RandomF32 {
+    pub const fn new(cnt: usize, seed: u64) -> Self {
+        let low = seed >> 32;
+        let hi = seed & 0xffffffff;
+
+        return Self(
+            [
+                0x6a09e667f3bcc908 ^ low,
+                0xbb67ae8584caa73b ^ hi,
+            ],
+            cnt,
+        );
+    }
+
+    /// xorshift128p
+    fn next_u32(&mut self) -> u32 {
+        let s = self.0[1];
+        let mut t = self.0[0];
+        self.0[0] = s;
+        t ^= t << 23;
+        t ^= t >> 18;
+        t ^= s ^ (s >> 5);
+        self.0[1] = t;
+        return t.wrapping_add(s) as u32;
+    }
+}
+
+impl Iterator for RandomF32 {
+    type Item = f32;
+    fn next(&mut self) -> Option<f32> {
+        if self.1 == 0 {
+            return None;
+        }
+        self.1 -= 1;
+        loop {
+            let bits = self.next_u32();
+            let exp = (bits >> 23) & 0xff;
+            // reject:
+            // exp == 0   → subnormal or zero
+            // exp == 255 → NaN / Inf
+            if exp == 0 || exp == 0xff {
+                continue;
+            }
+            return Some(f32::from_bits(bits));
+        }
+    }
+}

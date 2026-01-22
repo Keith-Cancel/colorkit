95fe568e45ee357708d52e779eefd571132007fd
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-22 11:14:28 -0800

    Update main.rs

--- test_math/src/main.rs
+++ test_math/src/main.rs
@@ -5,6 +5,9 @@ use colorkit::utils::math;
 
 fn main() {
     let p = runner::Perf::new();
+    let r = runner::Relative::new();
     p.run::<tests::Quirt>();
+    r.run::<tests::Quirt>();
     println!("{}", math::quirt(32.0));
+    println!("{}", math::quirt(243.0));
 }

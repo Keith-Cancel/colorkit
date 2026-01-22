c436f954a4706689c2376404d987e3c4460fde33
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-22 01:18:38 -0800

    Let the reference and std impl handle negatives.

--- test_math/src/tests/quirt.rs
+++ test_math/src/tests/quirt.rs
@@ -16,12 +16,16 @@ impl MathFn for Quirt {
     }
 
     fn std_f32_impl(x: f32) -> f32 {
-        return x.powf(0.2);
+        let neg = x < 0.0;
+        let x = if neg { -x } else { x };
+        let x = x.powf(0.2);
+        return if neg { -x } else { x };
     }
 
     fn rug_impl(prec: u32, x: f32) -> Float {
+        let neg = x < 0.0;
         let p = Float::with_val(prec, 5u32).recip();
-        let x = Float::with_val(prec, x);
-        return x.pow(p);
+        let x = Float::with_val(prec, x).abs().pow(p);
+        return if neg { -x } else { x };
     }
 }

c6e6050861d0378712a3f77bdd0a877270575b7d
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-21 22:06:14 -0800

    Update mod.rs

--- test_math/src/runner/mod.rs
+++ test_math/src/runner/mod.rs
@@ -1,8 +1,8 @@
 mod perf;
 mod primes;
 mod relative;
-
 pub use perf::Perf;
+pub use primes::PRIMES;
 pub use relative::Relative;
 
 pub struct Ansi;
@@ -16,4 +16,25 @@ impl Ansi {
     pub const BLUE: &'static str = "\x1b[34m";
     pub const MAGENTA: &'static str = "\x1b[35m";
     pub const CYAN: &'static str = "\x1b[36m";
+}
+
+pub struct PowersF32(u32);
+
+impl PowersF32 {
+    pub const fn new() -> Self {
+        return Self(0);
+    }
+}
+
+impl Iterator for PowersF32 {
+    type Item = f32;
+    fn next(&mut self) -> Option<f32> {
+        let b = self.0;
+        if b >= 0x7f800000 {
+            return None;
+        }
+        let n = f32::from_bits(b);
+        self.0 = b + 0x800000;
+        return Some(n);
+    }
 }

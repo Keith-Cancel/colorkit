b27c13c251f72eb7135aff8298da2717175206ec
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-22 19:23:00 -0800

    Update mod.rs

--- src/utils/math/mod.rs
+++ src/utils/math/mod.rs
@@ -7,6 +7,10 @@ pub use quirt::quirt_f32;
 /// This is to make up for the fact #[no_std] does
 /// have all math functions for f32, so I have reimplemented
 /// some for a no standard enviroment.
+///
+/// I have also have some tests that evaluate the accuracy and
+/// performance of these functions in the root of this repo
+/// called `test_math`
 pub trait MathFuncs {
     /// Computes the quintic root or 5th root.
     fn quirt(self) -> Self;

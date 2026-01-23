51d34a44f037d9124d894c7f3bdde4212b2b2c8e
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-22 19:47:33 -0800

    Update ulp.rs

---
+++ test_math/src/runner/ulp.rs
@@ -0,0 +1,13 @@
+use colorkit::utils::math::MathFuncs;
+
+fn ulp_diff(ref_: f64, b: f32) -> f64 {
+    if ref_.is_nan() || b.is_nan() {
+        return if ref_.is_nan() == b.is_nan() {
+            0.0
+        } else {
+            f64::INFINITY
+        };
+    }
+
+    todo!();
+}

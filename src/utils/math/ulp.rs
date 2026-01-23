4b8dc9c6ac3a657beb21e458cb88a13ae3ca35e3
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-22 20:10:05 -0800

    Update ulp.rs

--- src/utils/math/ulp.rs
+++ src/utils/math/ulp.rs
@@ -7,3 +7,31 @@ pub const fn ulp_int_diff_f32(a: f32, b: f32) -> u32 {
     let b = b.to_bits();
     return if a > b { a - b } else { b - a };
 }
+
+#[cfg(test)]
+mod test {
+    use super::ulp_int_diff_f32 as ulp;
+
+    #[test]
+    fn ulp_check() {
+        // rust's f32::MIN_POSITIVE is not the true minium
+        // the true mininum is a subnormal value.
+        let min = f32::from_bits(1);
+
+        assert_eq!(ulp(1.0, 1.0f32.next_up()), 1);
+        assert_eq!(ulp(1.0, 1.0f32.next_down()), 1);
+        assert_eq!(ulp(1.0, 1.0 + f32::EPSILON), 1);
+        assert_eq!(ulp(1.0, 1.0 - f32::EPSILON), 2);
+        assert_eq!(ulp(1.0, 1.0 - f32::EPSILON), 2);
+        assert_eq!(ulp(min, 0.0), 1);
+        assert_eq!(ulp(-min, min), 3);
+        assert_eq!(ulp(-1.0, -1.0 - f32::EPSILON), 1);
+        assert_eq!(ulp(-1.0, -1.0 + f32::EPSILON), 2);
+        assert_eq!(ulp(0.0, 0.0), 0);
+        assert_eq!(ulp(-0.0, 0.0), 1);
+        assert_eq!(ulp(f32::MAX, f32::INFINITY), 1);
+        assert_eq!(ulp(-f32::MAX, f32::NEG_INFINITY), 1);
+        assert_eq!(ulp(1.0, 0.5), 8388608);
+        assert_eq!(ulp(1.0, 2.0), 8388608);
+    }
+}

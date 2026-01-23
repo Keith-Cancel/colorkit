8e94bea6fc0e524aa8594fe59e08de50c9a3dbdd
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-23 13:42:13 -0800

    Add x86_64 sqrt

--- src/utils/math/arch/mod.rs
+++ src/utils/math/arch/mod.rs
@@ -1,2 +1,5 @@
 #[cfg(target_feature = "sse2")]
 mod x86_64;
+
+#[cfg(target_feature = "sse2")]
+pub use x86_64::sqrtf;
--- src/utils/math/arch/x86_64.rs
+++ src/utils/math/arch/x86_64.rs
@@ -1,2 +1,13 @@
+use core::arch::asm;
 
-
+#[inline(always)]
+pub fn sqrtf(x: f32) -> f32 {
+    let mut ret = x;
+    unsafe {
+        asm!(
+            "sqrtss {r}, {r}",
+            r = inout(xmm_reg) ret,
+        )
+    };
+    ret
+}

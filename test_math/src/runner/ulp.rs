3bfaccf5abb9e25ddc2e62128a2c2220fa3b14d8
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-23 00:22:54 -0800

    Update ulp.rs
    Update ulp.rs

--- test_math/src/runner/ulp.rs
+++ test_math/src/runner/ulp.rs
@@ -1,6 +1,5 @@
-use std::hint::black_box;
-
 use colorkit::utils::math::MathFuncs;
+use rug::Float;
 
 fn f64_to_f32_down(x: f64) -> f32 {
     let f = x as f32;
@@ -45,6 +44,12 @@ fn ulp_diff(ref_d: f64, x: f32) -> f64 {
     // or up.
     let ref_dwn = f64_to_f32_down(ref_d);
     let ulp_sz = (ref_dwn.next_up() - ref_dwn) as f64;
+    // Shouldn't happen?
+    // I guess maybe some kinda of hardware flush of a subnormal
+    if ulp_sz == 0.0 {
+        // Could also just pannic?
+        return ulp_i as f64;
+    }
 
     // The fractional part of the ulp
     let mut frac = (ref_d - (ref_f as f64)) / ulp_sz;

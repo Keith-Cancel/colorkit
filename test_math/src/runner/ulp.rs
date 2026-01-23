71c80f79c766017a4a91b42d3e3d804a7b8d8aa5
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-22 21:58:19 -0800

    Update ulp.rs

--- test_math/src/runner/ulp.rs
+++ test_math/src/runner/ulp.rs
@@ -1,14 +1,25 @@
 use colorkit::utils::math::MathFuncs;
 
-fn ulp_diff(ref_: f64, b: f32) -> f64 {
-    if ref_.is_nan() || b.is_nan() {
-        return if ref_.is_nan() == b.is_nan() {
+fn f64_to_f32_down(x: f64) -> f32 {
+    let f = x as f32;
+    return if (f as f64) > x { f.next_down() } else { f };
+}
+
+fn ulp_diff(ref_d: f64, x: f32) -> f64 {
+    if ref_d.is_nan() || x.is_nan() {
+        return if ref_d.is_nan() == x.is_nan() {
             0.0
         } else {
             f64::INFINITY
         };
     }
-    let int_ulp = (ref_ as f32).ulp_int_diff(b);
+    let ref_f = ref_d as f32;
+
+    // Integer part of the ulp.
+    let ulp_i = ref_f.ulp_int_diff(x);
+    let ref_d = ref_d.abs();
+    let ref_f = ref_f.abs();
+    let x = x.abs();
 
     todo!();
 }

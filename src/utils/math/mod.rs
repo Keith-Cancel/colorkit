32cadc2a209305244edc930a3bc22cb51a1d203a
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-22 19:17:48 -0800

    define a trait.

--- src/utils/math/mod.rs
+++ src/utils/math/mod.rs
@@ -1,1 +1,19 @@
 mod quirt;
+
+pub use quirt::quirt;
+
+/// Common math functions
+///
+/// This is to make up for the fact #[no_std] does
+/// have all math functions for f32, so I have reimplemented
+/// some for a no standard enviroment.
+pub trait MathFuncs {
+    /// Computes the quintic root or 5th root.
+    fn quirt(self) -> Self;
+}
+
+impl MathFuncs for f32 {
+    fn quirt(self) -> Self {
+        return quirt(self);
+    }
+}

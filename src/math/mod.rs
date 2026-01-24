80ec6c52f915063e4fd8af54299cb810dc9051be
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-24 01:30:34 -0800

    Have an alias to the universal for const.

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,9 +1,6 @@
 mod arch;
 mod universal;
 
-pub use universal::quirtf;
-pub use universal::ulp_int_diff_f32;
-
 // TODO: arch and const fns
 // Ideally all the math functions would be `const fn`, but that means
 // I can't use hardware features =(
@@ -15,6 +12,13 @@ pub use universal::ulp_int_diff_f32;
 // at the time of writing this sighs....
 // I guess in the mean time just add const version that
 // that calls the universal implementation.
+
+pub use universal::quirtf;
+pub use universal::ulp_int_diff_f32;
+
+#[rustfmt::skip]
+// Const functions
+pub use universal::sqrtf as sqrtf_const;
 
 /// Common math functions
 ///

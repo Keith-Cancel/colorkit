00f851e5c3bf71203ab3042b82fa91c57871d9dd
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-23 23:02:33 -0800

    Instead manually computing these consts make a function to do it.

--- src/math/universal/mod.rs
+++ src/math/universal/mod.rs
@@ -10,3 +10,22 @@ pub use ulp::ulp_int_diff_f32;
 pub const F32_MSK_EXP: u32 = 0x7f800000;
 /// The bias amount for an f32 exponent.
 pub const F32_BIAS: i32 = 127;
+
+/// Compute the const to add the floating point number after dividing
+/// for the intial guess.
+///
+/// For example if we divid by 5 for the 5th root.
+/// x = (e - 127)/5 + 127
+/// x = (e - 127)/5 + 635/5
+/// x = e/5 - 127/5 + 635/5
+/// x = e/5 + 508/5
+/// 508 / 5 ~= 0x65.999999 in a fixed point u32 with 24 bit fraction
+/// shift right 1 and it's then 0x32cccccc
+/// and add 1 acount for the shifted off bit.
+const fn root_const(minuend: u32, subtrahend: u32, divisor: u32) -> u32 {
+    let dif = ((minuend - subtrahend) as u64) << 32;
+    let frac = dif / divisor as u64;
+    let frac = (frac >> 8) as u32;
+    let rnd = frac & 1;
+    return (frac >> 1) + rnd;
+}

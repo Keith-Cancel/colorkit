f5e574b4af88418cd3e8af2c93f88994146f26dd
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-24 17:46:30 -0800

    Update arm64.rs

--- src/math/arch/arm64.rs
+++ src/math/arch/arm64.rs
@@ -5,11 +5,11 @@ pub fn sqrtf(x: f32) -> f32 {
     let mut ret = x;
     unsafe {
         asm!(
-            "fsqrt s0, s0",
-            //"fsqrt {r}, {r}",
-            //r = inout(??) ret,
+            "fsqrt {r:s}, {r:s}",
+            r = inout(vreg) ret,
+            // https://doc.rust-lang.org/reference/inline-assembly.html#r-asm.register-operands.value-type-constraints
             // https://doc.rust-lang.org/rust-by-example/unsafe/asm.html#options
-            //options(nomem, nostack, pure),
+            options(nomem, nostack, pure),
         )
     };
     return ret;

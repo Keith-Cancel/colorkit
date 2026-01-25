873938af87109752d551ed1f075c9a4b050bfb12
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-24 17:48:28 -0800

    Update mod.rs

--- src/math/arch/mod.rs
+++ src/math/arch/mod.rs
@@ -4,4 +4,8 @@ mod x86_64;
 #[cfg(target_feature = "sse2")]
 pub use x86_64::sqrtf;
 
+#[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
 mod arm64;
+
+#[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
+pub use arm64::sqrtf;

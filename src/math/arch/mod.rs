d5687d70be12e823374f25ce55a1755ce067cfc9
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-24 18:10:12 -0800

    Update mod.rs

--- src/math/arch/mod.rs
+++ src/math/arch/mod.rs
@@ -9,3 +9,17 @@ mod arm64;
 
 #[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
 pub use arm64::sqrtf;
+
+macro_rules! arch_fn {
+    (name: $name:ident, args: $($arg:expr),+) => {
+        colorkit::math::arch::arch_fn!(@inner $name, $($arg),+);
+    };
+    (@inner sqrtf, $($arg:expr),+) => {
+        #[cfg(any(
+            target_feature = "sse2",
+            all(target_arch = "aarch64", target_feature = "neon")
+        ))]
+        return colorkit::math::arch::sqrtf($($arg),+);
+    };
+}
+pub(crate) use arch_fn;

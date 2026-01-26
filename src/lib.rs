04716a31c3ef262dfedab169020d5b7de9f535fb
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-26 12:20:16 -0800

    Add colors module

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ pub use space2::ColorSpace;
 pub use space2::ColorArray;
 pub use space2::ColorTransmute;
 pub use space2::white_point as wp;
+pub mod colors;
 //pub mod channels;
 //pub mod layout;
 //pub mod scalar;

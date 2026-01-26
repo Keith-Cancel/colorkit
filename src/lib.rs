050c5dd305bcf8b984ba88f994299fd954553031
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-26 13:21:16 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ mod space2;
 pub use space2::ColorSpace;
 pub use space2::ColorArray;
 pub use space2::ColorTransmute;
+pub use space2::RgbLike;
 pub use space2::white_point as wp;
 pub mod colors;
 //pub mod channels;

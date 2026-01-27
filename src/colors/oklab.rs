054a62c8562092fc753cafb80f935074e3860428
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-27 14:39:37 -0800

    Update oklab.rs

--- src/colors/oklab.rs
+++ src/colors/oklab.rs
@@ -13,12 +13,24 @@ pub struct OkLab([f32; 3]);
 
 impl OkLab {
     /// Matrix used as part the conversion From XYZ
+    ///
+    /// https://bottosson.github.io/posts/oklab/
     #[rustfmt::skip]
     pub const M1: [f32; 9] = [
-        0.8189330101, 0.3618667424, 0.1288597137,
-        0.0329845436, 0.9293118715, 0.0361456387,
-        0.0482003018, 0.2643662691, 0.6338517070
+        0.8189330101, 0.3618667424, -0.1288597137,
+        0.0329845436, 0.9293118715,  0.0361456387,
+        0.0482003018, 0.2643662691,  0.6338517070
     ];
+    /// Matrix used as part the conversion From XYZ
+    ///
+    /// https://bottosson.github.io/posts/oklab/
+    #[rustfmt::skip]
+    pub const M2: [f32; 9] = [
+        0.2104542553,  0.7936177850, -0.0040720468,
+        1.9779984951, -2.4285922050, 0.4505937099,
+        0.0259040371,  0.7827717662, -0.8086757660,
+    ];
+
     /// Create a new color from `Lab` values.
     pub const fn new(l: f32, a: f32, b: f32) -> Self {
         return Self([l, a, b]);

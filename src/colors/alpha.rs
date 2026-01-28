98ecd6526f665a816c7244a80620faa62a246841
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-28 01:49:56 -0800

    Update alpha.rs

--- src/colors/alpha.rs
+++ src/colors/alpha.rs
@@ -6,55 +6,90 @@ use colorkit::space2::ColorTransmute;
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Alpha<S: ColorTransmute>(S, f32);
 
-impl<S: ColorTransmute> Alpha<S> {
-    const CHANNELS: usize = size_of::<Alpha<S>>() / size_of::<f32>();
-    const MAX: &'static [BoundF32] = &const {
-        // Just make this larger than likely needed can't use
-        // S or Self in the len of an array =(
-        let mut max = [BoundF32::Unbounded; 16];
-        let mut i = 0;
-        while i < S::CHANNEL_MAX.len() {
-            max[i] = S::CHANNEL_MAX[i];
-            i += 1;
+base_funcs!(Alpha);
+
+macro_rules! base_funcs {
+    ($name:ident) => {
+        impl<S: ColorTransmute> $name<S> {
+            const LEN: usize = S::CHANNELS + 1;
+
+            /// View alpha color as a slice reference.
+            #[inline]
+            pub const fn as_slice(&self) -> &[f32] {
+                // Safety:
+                // The ColorSpace S is marked as transmuteable
+                // We also mark the alpha wrapper as repr(C)
+                // If min_const_generic_args is stabilized this
+                // can be replaced with safe constructs.
+                let p = self as *const _ as *const f32;
+                return unsafe { core::slice::from_raw_parts(p, Self::LEN) };
+            }
+            /// View alpha color as a mutable slice
+            #[inline]
+            pub const fn as_mut_slice(&mut self) -> &mut [f32] {
+                let p = self as *mut _ as *mut f32;
+                // Safety:
+                // The ColorSpace S is marked as transmuteable
+                // We also mark the alpha wrapper as repr(C)
+                // If min_const_generic_args is stabilized this
+                // can be replaced with safe constructs.
+                return unsafe { core::slice::from_raw_parts_mut(p, Self::LEN) };
+            }
         }
-        max[i] = BoundF32::Include(1.0);
-        max
-    };
-    const MIN: &'static [BoundF32] = &const {
-        let mut arr = [BoundF32::Unbounded; 16];
-        let mut i = 0;
-        while i < S::CHANNEL_MIN.len() {
-            arr[i] = S::CHANNEL_MIN[i];
-            i += 1;
+
+        impl<S: ColorTransmute> Default for $name<S> {
+            fn default() -> Self {
+                return Self(S::DEFAULT, 1.0);
+            }
         }
-        arr[i] = BoundF32::Include(0.0);
-        arr
+
+        impl<S: ColorTransmute> AsRef<[f32]> for $name<S> {
+            fn as_ref(&self) -> &[f32] {
+                return self.as_slice();
+            }
+        }
+
+        impl<S: ColorTransmute> AsMut<[f32]> for $name<S> {
+            fn as_mut(&mut self) -> &mut [f32] {
+                return self.as_mut_slice();
+            }
+        }
+
+        impl<S: ColorTransmute> ColorData for $name<S> {
+            type WhitePoint = S::WhitePoint;
+            const DEFAULT: Self = Self(S::DEFAULT, 1.0);
+            const LINEAR: bool = S::LINEAR;
+            const CHANNEL_MAX: &'static [BoundF32] = { Self::MAX.split_at(Self::LEN).0 };
+            const CHANNEL_MIN: &'static [BoundF32] = { Self::MIN.split_at(Self::LEN).0 };
+        }
+
+        impl<S: ColorTransmute> $name<S> {
+            const MAX: &'static [BoundF32] = &const {
+                // Just make this larger than likely needed can't use
+                // S or Self in the len of an array =(
+                let mut max = [BoundF32::Unbounded; 16];
+                let mut i = 0;
+                while i < S::CHANNEL_MAX.len() {
+                    max[i] = S::CHANNEL_MAX[i];
+                    i += 1;
+                }
+                max[i] = BoundF32::Include(1.0);
+                max
+            };
+            const MIN: &'static [BoundF32] = &const {
+                let mut arr = [BoundF32::Unbounded; 16];
+                let mut i = 0;
+                while i < S::CHANNEL_MIN.len() {
+                    arr[i] = S::CHANNEL_MIN[i];
+                    i += 1;
+                }
+                arr[i] = BoundF32::Include(0.0);
+                arr
+            };
+        }
     };
-
-    const fn slice_ref(&self) -> &[f32] {
-        let p = self as *const _ as *const f32;
-        return unsafe { core::slice::from_raw_parts(p, Self::CHANNELS) };
-    }
-
-    const fn slice_mut(&mut self) -> &mut [f32] {
-        let p = self as *mut _ as *mut f32;
-        return unsafe { core::slice::from_raw_parts_mut(p, Self::CHANNELS) };
-    }
 }
-
-impl<S: ColorTransmute> Default for Alpha<S> {
-    fn default() -> Self {
-        return Self(S::DEFAULT, 1.0);
-    }
-}
-
-impl<S: ColorTransmute> ColorData for Alpha<S> {
-    type WhitePoint = S::WhitePoint;
-    const DEFAULT: Self = Self(S::DEFAULT, 1.0);
-    const LINEAR: bool = S::LINEAR;
-    const CHANNEL_MAX: &'static [BoundF32] = { Self::MAX.split_at(Self::CHANNELS).0 };
-    const CHANNEL_MIN: &'static [BoundF32] = { Self::MIN.split_at(Self::CHANNELS).0 };
-}
+pub(crate) use base_funcs;
 
 #[cfg(test)]
 mod test {

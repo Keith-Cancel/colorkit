d5622e1d4a673c6e02aac156f47f9d3e26338704
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-28 01:55:01 -0800

    Update mod.rs

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -31,7 +31,7 @@ pub use universal::sqrtf as sqrtf_const;
 
 /// Common math functions
 ///
-/// This is to make up for the fact #[no_std] does
+/// This is to make up for the fact #\[no_std\] does
 /// have all math functions for f32, so I have reimplemented
 /// some for a no standard enviroment.
 ///

06d663ddb3d0ac989570c145a36fb925eabce494
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-30 22:10:46 -0800

    I can get rid of this variable.

--- src/math/arch/x86_64.rs
+++ src/math/arch/x86_64.rs
@@ -1,29 +1,27 @@
 use core::arch::asm;
 
 #[inline(always)]
-pub fn sqrtf(x: f32) -> f32 {
-    let mut ret = x;
+pub fn sqrtf(mut x: f32) -> f32 {
     unsafe {
         asm!(
             "sqrtss {r}, {r}",
-            r = inout(xmm_reg) ret,
+            r = inout(xmm_reg) x,
             // https://doc.rust-lang.org/rust-by-example/unsafe/asm.html#options
             options(nomem, nostack, pure),
         )
     };
-    return ret;
+    return x;
 }
 
 #[inline(always)]
-pub fn truncf(x: f32) -> f32 {
-    let mut ret = x;
+pub fn truncf(mut x: f32) -> f32 {
     unsafe {
         asm!(
             "roundss {r}, {r}, 0x3",
-            r = inout(xmm_reg) ret,
+            r = inout(xmm_reg) x,
             // https://doc.rust-lang.org/rust-by-example/unsafe/asm.html#options
             options(nomem, nostack, pure),
         )
     };
-    return ret;
+    return x;
 }

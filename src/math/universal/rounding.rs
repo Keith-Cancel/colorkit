c0bd047001b772fe713f8192af779ffd1dce15fa
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-30 18:45:58 -0800

    Update rounding.rs

--- src/math/universal/rounding.rs
+++ src/math/universal/rounding.rs
@@ -1,3 +1,40 @@
+use super::exponentf;
+/// Get the integer part of the float. Truncates the fraction
+/// always to zero.
 pub const fn truncf(x: f32) -> f32 {
+    let bits = x.to_bits();
+    let neg = bits & 0x80000000;
+    let exp = exponentf(bits);
+
+    // Purely fractional so will just be zero.
+    if exp < 0 {
+        return f32::from_bits(neg);
+    }
+    todo!();
     return x;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    fn bit_eq(a: f32, b: f32) -> bool {
+        return a.to_bits() == b.to_bits();
+    }
+
+    #[test]
+    #[rustfmt::skip]
+    fn trunc_frac() {
+        assert!(bit_eq(truncf( 0.0),    0.0));
+        assert!(bit_eq(truncf(-0.0),   -0.0));
+        assert!(bit_eq(truncf( 0.75),   0.0));
+        assert!(bit_eq(truncf(-0.75),  -0.0));
+        assert!(bit_eq(truncf( 0.5),    0.0));
+        assert!(bit_eq(truncf(-0.5),   -0.0));
+        assert!(bit_eq(truncf( 0.25),   0.0));
+        assert!(bit_eq(truncf(-0.25),  -0.0));
+        assert!(bit_eq(truncf(2e-32),   0.0));
+        assert!(bit_eq(truncf(-2e-32), -0.0));
+        assert!(bit_eq(truncf(2e-45),   0.0));
+        assert!(bit_eq(truncf(-2e-45), -0.0));
+    }
 }

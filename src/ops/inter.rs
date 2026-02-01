106b63f043cf81c6d21d8cde3f52e7b1f4b7a374
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-01-31 23:34:49 -0800

    Update inter.rs

--- src/ops/inter.rs
+++ src/ops/inter.rs
@@ -1,5 +1,30 @@
-pub use colorkit::space::ColorSpace;
+use colorkit::scalar::NormF32;
+use colorkit::space::ColorSpace;
 
-pub trait Interpolation {}
+/// Extension trait for interpolation operations on colors
+pub trait Interpolation {
+    fn lerp_naive(&self, other: &Self, ratio: NormF32) -> Self;
+}
 
-impl<C: ColorSpace> Interpolation for C {}
+impl<C: ColorSpace> Interpolation for C {
+    /// Linearly interpolate between two colors.
+    ///
+    /// # Parameters
+    /// - `self`: Starting color (t = 0)
+    /// - `other`: Ending color (t = 1)
+    /// - `ratio`: Interpolation factor (0.0 to 1.0)
+    ///
+    /// # Note
+    /// It naively treats the alpha channel as any other channel.
+    ///
+    /// Further, performs channel-wise linear interpolation, this
+    /// works in any color space, but the perceptual result depends
+    /// on the space's linearity.
+    fn lerp_naive(&self, other: &Self, ratio: NormF32) -> Self {
+        return Self::from_fn(|i| {
+            let a = self[i];
+            let b = other[i];
+            return a + ratio * (b - a);
+        });
+    }
+}

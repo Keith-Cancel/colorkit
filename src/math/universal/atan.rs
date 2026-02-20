8bba84b6817d7216526e8e29b552726369718199
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-02-20 13:17:45 -0800

    Update atan.rs

--- src/math/universal/atan.rs
+++ src/math/universal/atan.rs
@@ -1,5 +1,51 @@
 use core::f64::consts::FRAC_PI_2;
 
+#[cfg(target_arch = "x86_64")]
+#[inline(always)]
+const fn mul_add(x: f64, a: f64, b: f64) -> f64 {
+    return x.mul_add(a, b);
+}
+
+#[cfg(not(target_arch = "x86_64"))]
+#[inline(always)]
+const fn mul_add(x: f64, a: f64, b: f64) -> f64 {
+    return x * a + c;
+}
+
+const fn atan_poly(x: f64) -> f64 {
+    let x_2 = x * x;
+
+    let p = mul_add(x_2, 15159.0, 147455.0);
+    let p = mul_add(x_2, p, 345345.0);
+    let p = mul_add(x_2, p, 225225.0);
+
+    let q = mul_add(x_2, 1225.0, 44100.0);
+    let q = mul_add(x_2, q, 242550.0);
+    let q = mul_add(x_2, q, 420420.0);
+    let q = mul_add(x_2, q, 225225.0);
+
+    return p / q;
+}
+
+pub const fn atanf2(x: f32) -> f32 {
+    let mut x_1 = x as f64;
+    let mut f = 0.0;
+
+    if x.abs() > 1.0 {
+        f = FRAC_PI_2.copysign(x_1);
+        x_1 = -x_1.recip();
+    }
+
+    if x.abs() >= 0.30209234 {
+        let c = (1.0 / 1.67f64).copysign(x_1);
+        f += 0.5395384432298387f64.copysign(x_1); // ~= arctan(c);
+        x_1 = (x_1 - c) / mul_add(x_1, c, 1.0);
+    }
+
+    let r = mul_add(x_1, atan_poly(x_1), f);
+    return r as f32;
+}
+
 pub const fn atanf(x: f32) -> f32 {
     let recip = x > 1.0 || x < -1.0;
     let x = x as f64;

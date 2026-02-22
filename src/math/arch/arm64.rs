8ead492d1c67662e7687b06632bca9dc833f62d4
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-02-22 10:36:07 -0800

    Update arm64.rs

--- src/math/arch/arm64.rs
+++ src/math/arch/arm64.rs
@@ -14,3 +14,20 @@ pub fn sqrtf(x: f32) -> f32 {
     };
     return ret;
 }
+
+#[inline(always)]
+pub fn fma(x: f64, a: f64, b: f64) -> f64 {
+    let mut ret = x;
+    unsafe {
+        asm!(
+            "fmadd {x:d}, {x:d}, {a:d}, {b:d}",
+            x = inout(vreg) ret,
+            a = in(vreg) a,
+            b = in(vreg) b,
+            // https://doc.rust-lang.org/reference/inline-assembly.html#r-asm.register-operands.value-type-constraints
+            // https://doc.rust-lang.org/rust-by-example/unsafe/asm.html#options
+            options(nomem, nostack, pure),
+        )
+    };
+    return ret;
+}

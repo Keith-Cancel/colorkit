6d65284c169962e3f1c7a42ee0a10f97964df29d
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-02-22 10:47:20 -0800

    mul_add is not available in no_std, use my fma_inner routine.

--- src/math/universal/atan.rs
+++ src/math/universal/atan.rs
@@ -1,34 +1,24 @@
 use core::f32;
 use core::f64;
 
-#[cfg(target_arch = "x86_64")]
-#[inline(always)]
-const fn mul_add(x: f64, a: f64, b: f64) -> f64 {
-    return x.mul_add(a, b);
-}
+use colorkit::math::fma_inner;
 
-#[cfg(not(target_arch = "x86_64"))]
-#[inline(always)]
-const fn mul_add(x: f64, a: f64, b: f64) -> f64 {
-    return x * a + b;
-}
-
-const fn atan_poly(x: f64) -> f64 {
+fn atan_poly(x: f64) -> f64 {
     let x_2 = x * x;
 
-    let p = mul_add(x_2, 15159.0, 147455.0);
-    let p = mul_add(x_2, p, 345345.0);
-    let p = mul_add(x_2, p, 225225.0);
+    let p = fma_inner(x_2, 15159.0, 147455.0);
+    let p = fma_inner(x_2, p, 345345.0);
+    let p = fma_inner(x_2, p, 225225.0);
 
-    let q = mul_add(x_2, 1225.0, 44100.0);
-    let q = mul_add(x_2, q, 242550.0);
-    let q = mul_add(x_2, q, 420420.0);
-    let q = mul_add(x_2, q, 225225.0);
+    let q = fma_inner(x_2, 1225.0, 44100.0);
+    let q = fma_inner(x_2, q, 242550.0);
+    let q = fma_inner(x_2, q, 420420.0);
+    let q = fma_inner(x_2, q, 225225.0);
 
     return p / q;
 }
 
-pub const fn atanf(x: f32) -> f32 {
+pub fn atanf(x: f32) -> f32 {
     let x_pos = x.abs();
     // When x is small enough it's just x.
     // Also preserves the sign of zero.
@@ -46,12 +36,12 @@ pub const fn atanf(x: f32) -> f32 {
 
     if x_pos > 6.0 {
         let x_2 = x_1 * x_1;
-        let p = mul_add(x_2, 1155.0, 1190.0);
-        let p = mul_add(x_2, p, 231.0);
+        let p = fma_inner(x_2, 1155.0, 1190.0);
+        let p = fma_inner(x_2, p, 231.0);
 
-        let q = mul_add(x_2, 1155.0, 1575.0);
-        let q = mul_add(x_2, q, 525.0);
-        let q = mul_add(x_2, q, 25.0);
+        let q = fma_inner(x_2, 1155.0, 1575.0);
+        let q = fma_inner(x_2, q, 525.0);
+        let q = fma_inner(x_2, q, 25.0);
 
         let r = x_1 * (p / q);
 
@@ -61,14 +51,14 @@ pub const fn atanf(x: f32) -> f32 {
     if x_pos >= 1.05 {
         let c = (1.0 / 0.48f64).copysign(x_1);
         f += 1.1232763516377267f64.copysign(x_1); // ~= arctan(c);
-        x_1 = (x_1 - c) / mul_add(x_1, c, 1.0);
+        x_1 = (x_1 - c) / fma_inner(x_1, c, 1.0);
     } else if x_pos >= 0.30209234 {
         let c = (1.0 / 1.67f64).copysign(x_1);
         f += 0.5395384432298387f64.copysign(x_1); // ~= arctan(c);
-        x_1 = (x_1 - c) / mul_add(x_1, c, 1.0);
+        x_1 = (x_1 - c) / fma_inner(x_1, c, 1.0);
     }
 
-    let r = mul_add(x_1, atan_poly(x_1), f);
+    let r = fma_inner(x_1, atan_poly(x_1), f);
     return r as f32;
 }
 

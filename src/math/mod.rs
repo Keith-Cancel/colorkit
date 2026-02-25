c49be2983de8ef6694be4e8f74e397a34322f82c
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-02-25 10:22:12 -0800

    Update mod.rs

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -85,7 +85,7 @@ pub use universal::truncf as truncf_const;
 ///
 /// This is to make up for the fact #\[no_std\] does
 /// have all math functions for f32, so I have reimplemented
-/// some for a no standard enviroment.
+/// some for a no standard environment.
 ///
 /// I have also have some tests that evaluate the accuracy and
 /// performance of these functions in the root of this repo

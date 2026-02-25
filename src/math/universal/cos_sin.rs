2b0227dd30cd64c26f5722da563d8bae4b02f012
Keith-Cancel <Keith-Cancel@users.noreply.github.com>
2026-02-25 02:45:39 -0800

    Making this one branch like this is faster.

--- src/math/universal/cos_sin.rs
+++ src/math/universal/cos_sin.rs
@@ -6,12 +6,28 @@ use colorkit::math::fma_inner;
 /// approximates tan(x) via P/Q for x in range 0.0..0.86345325
 #[inline(always)]
 fn tan_rational_poly(x_1: f64) -> (f64, f64) {
-    let shift = x_1 >= 0.4636476090008061; // ~= tan(0.4636476090008061) ~= 0.5
+    const C: f64 = 0.4636476090008061; // tan(C) ~= 0.5
+    // In this case x_1 = x - C
+    // use the identity: tan(a + b) = (tan(a) + tan(b))/(1 - tan(a)*tan(b))
+    // basically add the C back to x_1, but this lets me double the range.
+    if x_1 >= C {
+        let x_1 = x_1 - C;
+        let x_2 = x_1 * x_1;
 
-    // Take advantage of a tan identity to double the range.
-    let x_1 = if shift { x_1 - 0.4636476090008061 } else { x_1 };
+        // 10395x - 1260x^3 + 21x^5
+        let p = fma_inner(x_2, 21.0, -1260.0);
+        let p = fma_inner(x_2, p, 10395.0);
+        let p = x_1 * p;
+
+        // 10395 - 4725x^2 + 210x^4 - x^6
+        let q = 210.0 - x_2;
+        let q = fma_inner(x_2, q, -4725.0);
+        let q = fma_inner(x_2, q, 10395.0);
+
+        return (fma_inner(q, 0.5, p), fma_inner(p, -0.5, q));
+    }
+
     let x_2 = x_1 * x_1;
-
     // 10395x - 1260x^3 + 21x^5
     let p = fma_inner(x_2, 21.0, -1260.0);
     let p = fma_inner(x_2, p, 10395.0);
@@ -22,12 +38,6 @@ fn tan_rational_poly(x_1: f64) -> (f64, f64) {
     let q = fma_inner(x_2, q, -4725.0);
     let q = fma_inner(x_2, q, 10395.0);
 
-    // In this case x_1 = x - C0
-    // use the identity: tan(a + b) = (tan(a) + tan(b))/(1 - tan(a)*tan(b))
-    // basically add the C0 back to x_1
-    if shift {
-        return (fma_inner(q, 0.5, p), fma_inner(p, -0.5, q));
-    };
     return (p, q);
 }
 
